fb6a8ab333ac796b8a8ae72b7bfa5278fd1a644b
DevEssien <essienemma300@gmail.com>
2024-03-18 01:37:12 +0000

    added param validaton to endpoints of user

--- src/user_controller/validators/schemas.js
+++ src/user_controller/validators/schemas.js
@@ -1,8 +1,21 @@
-const { body, check } = require("express-validator");
+const { isValidObjectId } = require("mongoose");
+const { body, param } = require("express-validator");
+const User = require("../../database/repositories/user.repo");
+const { ValidationException } = require("../../libs/exceptions/index");
 
 exports.signupSchema = [
 	body("name").isString().trim(),
-	body("email").isString().trim().isEmail(),
+	body("email")
+		.isString()
+		.trim()
+		.isEmail()
+		.custom((value) => {
+			return User.getUserByEmail(value).then((user) => {
+				if (user) {
+					return Promise.reject("E-mail already in use");
+				}
+			});
+		}),
 	body("password")
 		.isString()
 		.isAlphanumeric()
@@ -17,4 +30,13 @@ exports.loginSchema = [
 		.isAlphanumeric()
 		.isLength({ min: 6 })
 		.withMessage("password must a string be of minimum length of 6"),
+];
+
+exports.endpointParamSchema = [
+	param("userId").customSanitizer((value) => {
+		const isValidId = isValidObjectId(value);
+		// if (!isValidId) return Promise.reject("User ID is invalid objectId. check user ID!"); // remember to fix the issue with this line
+		if (!isValidId) throw new ValidationException("User ID is invalid objectId. check user ID!");
+		return value;
+	}),
 ];
